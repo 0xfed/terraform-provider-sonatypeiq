3c8c21ee774cd62b3aa0220171d9861bb5e7cea2
Hung N. <0xfed@users.noreply.github.com>
2024-08-30 09:03:37 +0000

    chore: fmt and clean-up

--- examples/demo/main.tf
+++ examples/demo/main.tf
@@ -9,7 +9,7 @@ locals {
     for file in fileset("${path.module}/config/users/", "*.yaml") :
     trimsuffix(file, ".yaml") => yamldecode(file("${path.module}/config/users/${file}"))
   }
-  users =  flatten([for file in local.users_files: file])
+  users = flatten([for file in local.users_files : file])
   users_config = {
     for user in local.users :
     user.username => {
@@ -25,12 +25,12 @@ locals {
     for file in fileset("${path.module}/config/organizations/", "*.yaml") :
     trimsuffix(file, ".yaml") => yamldecode(file("${path.module}/config/organizations/${file}"))
   }
-  orgs =  flatten([for file in local.orgs_files: file])
+  orgs = flatten([for file in local.orgs_files : file])
   orgs_config = {
     for org in local.orgs :
     org.name => {
-        name                   = org.name
-        parent_organization_name = lookup(org, "parent", "Root Organization")
+      name                     = org.name
+      parent_organization_name = lookup(org, "parent", "Root Organization")
     }
   }
 
@@ -43,12 +43,12 @@ locals {
     for file in fileset("${path.module}/config/applications/", "*.yaml") :
     trimsuffix(file, ".yaml") => yamldecode(file("${path.module}/config/applications/${file}"))
   }
-  apps =  flatten([for file in local.apps_files: file])
+  apps = flatten([for file in local.apps_files : file])
   apps_config = {
     for app in local.apps :
     app.name => {
-        name                   = app.name
-        organization_name = app.parent
+      name              = app.name
+      organization_name = app.parent
     }
   }
 
@@ -66,13 +66,13 @@ locals {
 
 # 
 resource "local_file" "generated" {
-  content              = templatefile("${path.module}/templates/main.tftpl", {
+  content = templatefile("${path.module}/templates/main.tftpl", {
     users_config : local.users_config,
     orgs_config : local.orgs_config,
-    data_org_config: local.data_org_config,
+    data_org_config : local.data_org_config,
     apps_config : local.apps_config,
-    data_app_config: local.data_app_config,
-    })
+    data_app_config : local.data_app_config,
+  })
   filename             = "./generated.tf"
   directory_permission = "0777"
   file_permission      = "0777"
